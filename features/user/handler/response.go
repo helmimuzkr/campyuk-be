f5676215f8b4d51b2935b5e9d7415ebb0f448012
kgriffinh <griffinh29@gmail.com>
2023-02-02 18:12:15 +0800

    chore: add response user

--- features/user/handler/response.go
+++ features/user/handler/response.go
@@ -1,1 +1,21 @@
 package handler
+
+import "campyuk-api/features/user"
+
+type UserReponse struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+	Fullname string `json:"fullname"`
+	Email    string `json:"email"`
+	Role     string `json:"role"`
+}
+
+func ToResponse(data user.Core) UserReponse {
+	return UserReponse{
+		ID:       data.ID,
+		Username: data.Username,
+		Fullname: data.Fullname,
+		Email:    data.Email,
+		Role:     data.Role,
+	}
+}
