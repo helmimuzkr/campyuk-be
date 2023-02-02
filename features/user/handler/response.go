e6e0f8c5a011b65a24cbc4e4dfe93b5a3bc4c070
kgriffinh <griffinh29@gmail.com>
2023-02-02 18:31:53 +0800

    chore: add handler user

--- features/user/handler/response.go
+++ features/user/handler/response.go
@@ -3,11 +3,12 @@ package handler
 import "campyuk-api/features/user"
 
 type UserReponse struct {
-	ID       uint   `json:"id"`
-	Username string `json:"username"`
-	Fullname string `json:"fullname"`
-	Email    string `json:"email"`
-	Role     string `json:"role"`
+	ID        uint   `json:"id"`
+	Username  string `json:"username"`
+	Fullname  string `json:"fullname"`
+	Email     string `json:"email"`
+	UserImage string `json:"user_image"`
+	Role      string `json:"role"`
 }
 
 func ToResponse(data user.Core) UserReponse {
@@ -17,5 +18,16 @@ func ToResponse(data user.Core) UserReponse {
 		Fullname: data.Fullname,
 		Email:    data.Email,
 		Role:     data.Role,
+	}
+}
+
+func GetToResponse(data user.Core) UserReponse {
+	return UserReponse{
+		ID:        data.ID,
+		Username:  data.Username,
+		Fullname:  data.Fullname,
+		Email:     data.Email,
+		UserImage: data.UserImage,
+		Role:      data.Role,
 	}
 }
