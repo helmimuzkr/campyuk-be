9d0cb39e6df8796c31f5dc8346321ca20c5d12b7
helmimuzkr <mhelmimuzakir@gmail.com>
2023-02-03 19:22:39 +0700

    chore: change library validator to v10 and mid log

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 
 	"log"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -24,11 +25,12 @@ func main() {
 	db := config.InitDB(*cfg)
 	config.Migrate(db)
 
-	// v := validator.New()
+	v := validator.New()
 	// cld := config.NewCloudinary(*cfg)
 
 	config.Migrate(db)
 
+	// SETUP DOMAIN
 	uData := usrData.New(db)
 	uSrv := usrSrv.New(uData)
 	uHdl := usrHdl.New(uSrv)
@@ -38,26 +40,26 @@ func main() {
 	iHdl := itmHdl.New(iSrv)
 
 	campData := _campData.New(db)
-	campSrv := _campService.New(campData)
+	campSrv := _campService.New(campData, v)
 	campHandler := _campHandler.New(campSrv)
 
+	// MIDDLEWARE
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.CORS())
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "method=${method}, uri=${uri}, status=${status}, error=${error}\n",
+		Format:           "${time_custom}, method=${method}, uri=${uri}, status=${status}\n",
+		CustomTimeFormat: "2006-01-02 15:04:05",
 	}))
 
-	// user
+	// ROUTE
 	e.POST("/register", uHdl.Register())
 	e.POST("/login", uHdl.Login())
 	e.GET("/users", uHdl.Profile(), middleware.JWT([]byte(config.JWT_KEY)))
 	e.PUT("/users", uHdl.Update(), middleware.JWT([]byte(config.JWT_KEY)))
 	e.DELETE("/users", uHdl.Delete(), middleware.JWT([]byte(config.JWT_KEY)))
 
-	// camp
 	e.POST("/camps", campHandler.Add(), middleware.JWT([]byte(config.JWT_KEY)))
 
-	// item
 	e.POST("/items", iHdl.Add(), middleware.JWT([]byte(config.JWT_KEY)))
 	e.PUT("/items/:id", iHdl.Update(), middleware.JWT([]byte(config.JWT_KEY)))
 	e.DELETE("/items/:id", iHdl.Delete(), middleware.JWT([]byte(config.JWT_KEY)))
