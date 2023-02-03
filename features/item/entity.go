375cff224951b0492cdd646ddec293bf803fb061
kgriffinh <griffinh29@gmail.com>
2023-02-03 15:21:29 +0800

    chore: add entity item

--- features/item/entity.go
+++ features/item/entity.go
@@ -1,1 +1,29 @@
 package item
+
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	ID     uint
+	Name   string
+	Stock  int
+	Price  int
+	CampID int
+}
+
+type ItemHandler interface {
+	Add() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+}
+
+type ItemService interface {
+	Add(token interface{}, campID uint, newItem Core) (Core, error)
+	Update(token interface{}, itemID uint, updateData Core) (Core, error)
+	Delete(token interface{}, itemID uint) error
+}
+
+type ItemData interface {
+	Add(userID uint, newItem Core) (Core, error)
+	Update(userID uint, itemID uint, updateData Core) (Core, error)
+	Delete(userID uint, itemID uint) error
+}
