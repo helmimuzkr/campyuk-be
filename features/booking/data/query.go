729190442249336d75987461fc192dc70267b6e8
kgriffinh <griffinh29@gmail.com>
2023-02-04 15:54:17 +0800

    chore: func name revision

--- features/booking/data/query.go
+++ features/booking/data/query.go
@@ -16,22 +16,22 @@ func New(db *gorm.DB) booking.BookingData {
 	}
 }
 
-func (bq *bookingData) Create(userID uint, virtualNumber string) (booking.Core, error) {
+func (bd *bookingData) Create(userID uint, virtualNumber string) (booking.Core, error) {
 	return booking.Core{}, nil
 }
 
-func (bq *bookingData) Update(bookingID uint) (booking.Core, error) {
+func (bd *bookingData) Update(bookingID uint) (booking.Core, error) {
 	return booking.Core{}, nil
 }
 
-func (bq *bookingData) List(userID uint) ([]booking.Core, error) {
+func (bd *bookingData) List(userID uint) ([]booking.Core, error) {
 	return []booking.Core{}, nil
 }
 
-func (bq *bookingData) GetByID(userID uint, bookingID uint) (booking.Core, error) {
+func (bd *bookingData) GetByID(userID uint, bookingID uint) (booking.Core, error) {
 	return booking.Core{}, nil
 }
 
-func (bq *bookingData) Cancel(userID uint, bookingID uint) (booking.Core, error) {
+func (bd *bookingData) Cancel(userID uint, bookingID uint) (booking.Core, error) {
 	return booking.Core{}, nil
 }
