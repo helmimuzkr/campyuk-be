903d755f784af56a751e69208c57aff89dd475e7
kgriffinh <griffinh29@gmail.com>
2023-02-04 15:32:53 +0800

    chore: add entity booking

--- features/booking/entity.go
+++ features/booking/entity.go
@@ -1,1 +1,42 @@
 package booking
+
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	ID            uint
+	Checkin       string
+	Checkout      string
+	BookingDate   string
+	Guest         int
+	CampCost      int
+	TotalPrice    int
+	Status        string
+	Bank          string
+	VirtualNumber string
+}
+
+type BookingHandler interface {
+	Create() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	List() echo.HandlerFunc
+	GetByID() echo.HandlerFunc
+	Accept() echo.HandlerFunc
+	Cancel() echo.HandlerFunc
+}
+
+type BookingService interface {
+	Create(token interface{})
+	Update(token interface{})
+	List(token interface{}) ([]Core, error)
+	GetByID(token interface{}, bookingID uint) (Core, error)
+	Accept()
+	Cancel(token interface{}, bookingID uint) (Core, error)
+}
+
+type BookingData interface {
+	Create(userID uint, virtualNumber string) (Core, error)
+	Update(bookingID uint) (Core, error)
+	List(userID uint) ([]Core, error)
+	GetByID(userID uint, bookingID uint) (Core, error)
+	Cancel(userID uint, bookingID uint) (Core, error)
+}
