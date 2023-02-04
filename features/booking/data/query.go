344d6b37a629a265e5a474c86b6f04b53f6b094d
kgriffinh <griffinh29@gmail.com>
2023-02-04 15:49:46 +0800

    chore: add query booking

--- features/booking/data/query.go
+++ features/booking/data/query.go
@@ -1,1 +1,37 @@
 package data
+
+import (
+	"campyuk-api/features/booking"
+
+	"gorm.io/gorm"
+)
+
+type bookingQuery struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) booking.BookingData {
+	return &bookingQuery{
+		db: db,
+	}
+}
+
+func (bq *bookingQuery) Create(userID uint, virtualNumber string) (booking.Core, error) {
+	return booking.Core{}, nil
+}
+
+func (bq *bookingQuery) Update(bookingID uint) (booking.Core, error) {
+	return booking.Core{}, nil
+}
+
+func (bq *bookingQuery) List(userID uint) ([]booking.Core, error) {
+	return []booking.Core{}, nil
+}
+
+func (bq *bookingQuery) GetByID(userID uint, bookingID uint) (booking.Core, error) {
+	return booking.Core{}, nil
+}
+
+func (bq *bookingQuery) Cancel(userID uint, bookingID uint) (booking.Core, error) {
+	return booking.Core{}, nil
+}
