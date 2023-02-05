754a1d01ae33aad76e43ea85c4cb98aafd112d10
helmimuzkr <mhelmimuzakir@gmail.com>
2023-02-05 15:54:33 +0700

    chore: delete config parameter in new cloudinary

--- config/cloudinary.go
+++ config/cloudinary.go
@@ -6,7 +6,7 @@ import (
 	"github.com/cloudinary/cloudinary-go/v2"
 )
 
-func NewCloudinary(c AppConfig) *cloudinary.Cloudinary {
+func NewCloudinary() *cloudinary.Cloudinary {
 	cld, err := cloudinary.NewFromParams(CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET)
 	if err != nil {
 		log.Println("init cloudinary gagal", err)
