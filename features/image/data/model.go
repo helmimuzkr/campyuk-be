ffdd165b052dc99bf3aa7fbdfe87843678ff114b
helmimuzkr <mhelmimuzakir@gmail.com>
2023-02-06 02:58:23 +0700

    feat: add and delete image done

---
+++ features/image/data/model.go
@@ -0,0 +1,29 @@
+package data
+
+import (
+	"campyuk-api/features/image"
+
+	"gorm.io/gorm"
+)
+
+type Image struct {
+	gorm.Model
+	CampID uint
+	Image  string
+}
+
+func ToData(data image.Core) Image {
+	return Image{
+		Model:  gorm.Model{ID: data.ID},
+		CampID: uint(data.CampID),
+		Image:  data.Image,
+	}
+}
+
+func ToCore(data Image) image.Core {
+	return image.Core{
+		ID:     data.ID,
+		CampID: uint(data.CampID),
+		Image:  data.Image,
+	}
+}
