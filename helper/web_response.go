e2614cfadb6054073a22e440fff6aebc004869c5
helmimuzkr <mhelmimuzakir@gmail.com>
2023-02-07 01:20:15 +0700

    chore: add struct response with pagination

--- helper/web_response.go
+++ helper/web_response.go
@@ -5,6 +5,20 @@ import (
 	"strings"
 )
 
+type PaginationResponse struct {
+	Page        int `json:"page"`
+	Limit       int `json:"limit"`
+	Offset      int `json:"offset"`
+	TotalRecord int `json:"total_rercord"`
+	TotalPage   int `json:"total_page"`
+}
+
+type WithPagination struct {
+	Pagination PaginationResponse `json:"pagination"`
+	Data       interface{}        `json:"data"`
+	Message    string             `json:"message"`
+}
+
 func SuccessResponse(code int, message string, data ...any) (int, map[string]interface{}) {
 	response := make(map[string]interface{})
 
