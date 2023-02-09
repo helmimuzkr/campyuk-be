d45721ada662655b4cbf742371fd388cedcefde4
helmimuzkr <mhelmimuzakir@gmail.com>
2023-02-09 16:44:17 +0700

    test: overall cover test 94.4%

--- features/booking/service/service_test.go
+++ features/booking/service/service_test.go
@@ -11,14 +11,16 @@ import (
 
 	"github.com/golang-jwt/jwt"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/oauth2"
 )
 
-func setupTest(t *testing.T) (*mocks.PaymentGateway, *mocks.BookingData, booking.BookingService) {
+func setupTest(t *testing.T) (*mocks.PaymentGateway, *mocks.BookingData, booking.BookingService, *mocks.GoogleAPI) {
 	midtrans := mocks.NewPaymentGateway(t)
 	data := mocks.NewBookingData(t)
-	srv := New(data, midtrans)
+	google := mocks.NewGoogleAPI(t)
+	srv := New(data, midtrans, google)
 
-	return midtrans, data, srv
+	return midtrans, data, srv, google
 }
 
 func dataSample() (booking.Core, booking.Core) {
@@ -28,13 +30,13 @@ func dataSample() (booking.Core, booking.Core) {
 		Items: []booking.Item{
 			{ID: uint(2), Quantity: 1, RentCost: 50000},
 		},
-		CheckIn:       "01-02-2023",
-		CheckOut:      "02-02-2023",
+		CheckIn:       "2023-02-09",
+		CheckOut:      "2023-02-10",
 		Guest:         1,
 		CampCost:      20000,
 		TotalPrice:    70000,
 		Status:        "PENDING",
-		BookingDate:   time.Now().Format("02-01-2006"),
+		BookingDate:   time.Now().Format("2006-01-02"),
 		Bank:          "bca",
 		VirtualNumber: "90316950939",
 	}
@@ -53,13 +55,13 @@ func dataSample() (booking.Core, booking.Core) {
 		Items: []booking.Item{
 			{Name: "Small tent", Price: 500000, Quantity: 1, RentCost: 50000},
 		},
-		CheckIn:       "01-02-2023",
-		CheckOut:      "02-02-2023",
+		CheckIn:       "2023-02-09",
+		CheckOut:      "2023-02-10",
 		Guest:         1,
 		CampCost:      20000,
 		TotalPrice:    70000,
 		Status:        "PENDING",
-		BookingDate:   time.Now().Format("02-01-2006"),
+		BookingDate:   time.Now().Format("2006-01-02"),
 		Bank:          "bca",
 		VirtualNumber: "90316950939",
 	}
@@ -68,7 +70,7 @@ func dataSample() (booking.Core, booking.Core) {
 }
 
 func TestCreateBooking(t *testing.T) {
-	midtrans, data, srv := setupTest(t)
+	midtrans, data, srv, _ := setupTest(t)
 	inData, resData := dataSample()
 
 	t.Run("Succress create new order", func(t *testing.T) {
@@ -133,7 +135,7 @@ func TestCreateBooking(t *testing.T) {
 }
 
 func TestList(t *testing.T) {
-	_, data, srv := setupTest(t)
+	_, data, srv, _ := setupTest(t)
 	_, resData := dataSample()
 	listData := []booking.Core{resData, resData}
 	listData[0].ID = 1
@@ -185,7 +187,7 @@ func TestList(t *testing.T) {
 }
 
 func TestGetByID(t *testing.T) {
-	_, data, srv := setupTest(t)
+	_, data, srv, _ := setupTest(t)
 	_, resData := dataSample()
 
 	t.Run("Succress show booking detail", func(t *testing.T) {
@@ -250,7 +252,7 @@ func TestGetByID(t *testing.T) {
 }
 
 func TestAccept(t *testing.T) {
-	_, data, srv := setupTest(t)
+	_, data, srv, _ := setupTest(t)
 
 	t.Run("Succress accept order", func(t *testing.T) {
 		data.On("Update", uint(1), "host", uint(1), "SUCCESS").Return(nil).Once()
@@ -320,7 +322,7 @@ func TestAccept(t *testing.T) {
 }
 
 func TestCancel(t *testing.T) {
-	_, data, srv := setupTest(t)
+	_, data, srv, _ := setupTest(t)
 
 	t.Run("Succress cancel order", func(t *testing.T) {
 		data.On("Update", uint(1), "host", uint(1), "CANCEL").Return(nil).Once()
@@ -376,7 +378,7 @@ func TestCancel(t *testing.T) {
 }
 
 func TestCallback(t *testing.T) {
-	_, data, srv := setupTest(t)
+	_, data, srv, _ := setupTest(t)
 	inData, _ := dataSample()
 
 	t.Run("Succress callback ", func(t *testing.T) {
@@ -404,5 +406,90 @@ func TestCallback(t *testing.T) {
 		assert.NotNil(t, err)
 		assert.ErrorContains(t, err, "internal server error")
 		data.AssertExpectations(t)
+	})
+}
+
+func TestCreateEvent(t *testing.T) {
+	_, data, srv, google := setupTest(t)
+	_, resData := dataSample()
+
+	startTime, _ := time.Parse("2006-01-02", resData.CheckIn)
+
+	endTime, _ := time.Parse("2006-01-02", resData.CheckOut)
+
+	startRFC := startTime.Format(time.RFC3339)
+	endRFC := endTime.Format(time.RFC3339)
+
+	resGoogle := &oauth2.Token{}
+	detailCal := helper.CalendarDetail{
+		Summay:   "Camping",
+		Location: resData.Address,
+		Start:    startRFC,
+		End:      endRFC,
+		// nanti diisi email guest dan host
+		Emails: []string{resData.Email}, // email guest
+	}
+
+	t.Run("Success create event in calendar", func(t *testing.T) {
+		google.On("GetToken", "code").Return(resGoogle, nil).Once()
+		data.On("CreateEvent", resData.ID).Return(resData, nil).Once()
+		google.On("CreateCalendar", resGoogle, detailCal).Return(nil).Once()
+
+		err := srv.CreateEvent("code", resData.ID)
+
+		assert.Nil(t, err)
+		data.AssertExpectations(t)
+		google.AssertExpectations(t)
+	})
+	t.Run("Failed to get token", func(t *testing.T) {
+		google.On("GetToken", "code").Return(nil, errors.New("failed")).Once()
+		// data.On("CreateEvent", resData.ID).Return(resData, nil).Once()
+		// google.On("CreateCalendar", resGoogle, detailCal).Return(nil).Once()
+
+		err := srv.CreateEvent("code", resData.ID)
+
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "failed to create event in calendar")
+		data.AssertExpectations(t)
+		google.AssertExpectations(t)
+	})
+
+	t.Run("booking not found", func(t *testing.T) {
+		google.On("GetToken", "code").Return(resGoogle, nil).Once()
+		data.On("CreateEvent", resData.ID).Return(booking.Core{}, errors.New("not found")).Once()
+		// google.On("CreateCalendar", resGoogle, detailCal).Return(nil).Once()
+
+		err := srv.CreateEvent("code", resData.ID)
+
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "booking not found")
+		data.AssertExpectations(t)
+		google.AssertExpectations(t)
+	})
+
+	t.Run("database error", func(t *testing.T) {
+		google.On("GetToken", "code").Return(resGoogle, nil).Once()
+		data.On("CreateEvent", resData.ID).Return(booking.Core{}, errors.New("query error")).Once()
+		// google.On("CreateCalendar", resGoogle, detailCal).Return(nil).Once()
+
+		err := srv.CreateEvent("code", resData.ID)
+
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "internal server error")
+		data.AssertExpectations(t)
+		google.AssertExpectations(t)
+	})
+
+	t.Run("database error", func(t *testing.T) {
+		google.On("GetToken", "code").Return(resGoogle, nil).Once()
+		data.On("CreateEvent", resData.ID).Return(resData, nil).Once()
+		google.On("CreateCalendar", resGoogle, detailCal).Return(errors.New("failed to create")).Once()
+
+		err := srv.CreateEvent("code", resData.ID)
+
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "failed to create event in calendar")
+		data.AssertExpectations(t)
+		google.AssertExpectations(t)
 	})
 }
